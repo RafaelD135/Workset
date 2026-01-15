db45c75a3026a6a3533f236f4325d3ab755c2481
RafaelD135 <debeinrafael@gmail.com>
2026-01-15 16:44:18 +0100

    Code de test d'éxécution des tâches par défauts

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,58 @@
 import "./App.css";
 
+import { invoke } from '@tauri-apps/api/core';
+
 import { Button } from "@chakra-ui/react"
-import { HStack } from "@chakra-ui/react";
-import { RiArrowRightLine, RiMailLine } from "react-icons/ri";
 
 function App() {
-  return (
-    <HStack>
-      <Button colorPalette="teal" variant="solid">
-        <RiMailLine /> Email
-      </Button>
-      <Button colorPalette="teal" variant="outline">
-        Call us <RiArrowRightLine />
-      </Button>
-    </HStack>
-  )
+	const tasks = [
+		{
+			name: "Lancer Projet Web",
+			config: {
+				type: "Internet",
+				params: {
+					url: "https://youtube.com"
+				}
+			}
+		},
+		{
+			name: "Ouvrir VSCode", config: {
+				type: "VsCode",
+				params: {
+					path: "C:/Users/debei/Workset/src-tauri"
+				}
+			}
+		},
+		{
+			name: "Ouvrir Terminal", config: {
+				type: "Terminal",
+				params: {
+					path: "C:/Users/debei/Workset/src-tauri",
+					command: "ls"
+				}
+			}
+		},
+		{
+			name: "Ouvrir Explorateur de fichiers", config: {
+				type: "FileExplorer",
+				params: {
+					path: "C:/Users/debei/Workset/src-tauri",
+				}
+			}
+		}
+	];
+
+	return (
+		<div style={{ display: 'flex', flexDirection: 'column', gap: '10px', padding: '20px' }}>
+			{tasks.map(task => (
+				<Button key={task.name} onClick={async () => {
+					await invoke('launch_task', { task });
+				}}>
+					{task.name}
+			</Button>
+			))}
+		</div>
+	);
 }
 
 export default App;
