0b99177a0d2f4a7746cbb619120e497921be9a5f
RafaelD135 <debeinrafael@gmail.com>
2026-02-07 15:03:34 +0100

    Ajout écart entre params dans ParamTerminal

--- src/components/tasks/createParam/ParamTerminal.tsx
+++ src/components/tasks/createParam/ParamTerminal.tsx
@@ -1,21 +1,25 @@
-import { Box, Input, Text } from "@chakra-ui/react";
+import { Box, Input, Text, VStack } from "@chakra-ui/react";
 
 export default function ParamTerminal({ value, onChange }: any) {
 	return (
-		<Box>
-			<Text mb={2} fontSize="sm" fontWeight="medium">Chemin de lancement</Text>
-			<Input
-				placeholder="C:/Users/Projets/MonApp"
-				value={value.path || ""}
-				onChange={(e) => onChange({ ...value, path: e.target.value })}
-			/>
-		
-			<Text mb={2} fontSize="sm" fontWeight="medium">Commande à exécuter</Text>
-			<Input
-				placeholder="npm run dev"
-				value={value.command || ""}
-				onChange={(e) => onChange({ ...value, command: e.target.value })}
-			/>
-		</Box>
+		<VStack align="stretch">
+			<Box>
+				<Text mb={2} fontSize="sm" fontWeight="medium">Chemin de lancement</Text>
+				<Input
+					placeholder="C:/Users/Projets/MonApp"
+					value={value.path || ""}
+					onChange={(e) => onChange({ ...value, path: e.target.value })}
+				/>
+			</Box>
+
+			<Box>
+				<Text mb={2} fontSize="sm" fontWeight="medium">Commande à exécuter</Text>
+				<Input
+					placeholder="npm run dev"
+					value={value.command || ""}
+					onChange={(e) => onChange({ ...value, command: e.target.value })}
+				/>
+			</Box>
+		</VStack>
 	);
 }
