2c3961be09e4ff9c3be7cd54ca72c16424585f7d
RafaelD135 <debeinrafael@gmail.com>
2026-02-07 14:41:25 +0100

    Ajout de path a terminal

--- src/components/tasks/createParam/ParamTerminal.tsx
+++ src/components/tasks/createParam/ParamTerminal.tsx
@@ -3,6 +3,13 @@ import { Box, Input, Text } from "@chakra-ui/react";
 export default function ParamTerminal({ value, onChange }: any) {
 	return (
 		<Box>
+			<Text mb={2} fontSize="sm" fontWeight="medium">Chemin de lancement</Text>
+			<Input
+				placeholder="C:/Users/Projets/MonApp"
+				value={value.path || ""}
+				onChange={(e) => onChange({ ...value, path: e.target.value })}
+			/>
+		
 			<Text mb={2} fontSize="sm" fontWeight="medium">Commande à exécuter</Text>
 			<Input
 				placeholder="npm run dev"
